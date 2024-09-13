15285c90894360fc3d7a9c59e25f2d1803538f6e
RnG <113661821+okon0miyaki@users.noreply.github.com>
2024-09-13 18:28:57 +0700

    Update true_stretched.py
    using message box instead of print, so user know which function running

--- true_stretched.py
+++ true_stretched.py
@@ -7,7 +7,7 @@ window_title = "VALORANT  "
 window_handle = ctypes.windll.user32.FindWindowW(None, window_title)
 
 if window_handle == 0:
-    print("Valorant not found")
+    ctypes.windll.user32.MessageBoxW(0, "Valorant not Running", "EZTS", 0)
 else:
     # Change window properties
     style = ctypes.windll.user32.GetWindowLongW(window_handle, ctypes.c_int(-16))
@@ -18,4 +18,4 @@ else:
     # Maximize window
     ctypes.windll.user32.ShowWindow(window_handle, ctypes.c_int(3))  # SW_MAXIMIZE
 
-    print("True stretched applied")
+    ctypes.windll.user32.MessageBoxW(0, "True Stretch Applied", "EZTS", 0)
